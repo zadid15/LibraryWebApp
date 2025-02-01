e537150738e296faf8fc709bec3d4431e476a6a7
zadid15 <muhammadzadid061512@gmail.com>
2025-02-01 21:13:21 +0700

    Added BorrowingPolicy for authorization management

---
+++ app/Policies/BorrowingPolicy.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Policies;
+
+use App\Models\Borrowing;
+use App\Models\User;
+use Illuminate\Auth\Access\Response;
+
+class BorrowingPolicy
+{
+    /**
+     * Determine whether the user can view any models.
+     */
+    public function viewAny(User $user): Response
+    {
+        return in_array($user->role, ['admin', 'staff']) && $user->status === 'active'
+            ? Response::allow()
+            : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can view the model.
+     */
+    public function view(User $user, Borrowing $borrowing): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can create models.
+     */
+    public function create(User $user): Response
+    {
+        return in_array($user->role, ['admin', 'staff']) && $user->status === 'active'
+            ? Response::allow()
+            : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can update the model.
+     */
+    public function update(User $user): Response
+    {
+        return in_array($user->role, ['admin', 'staff']) && $user->status === 'active'
+            ? Response::allow()
+            : Response::deny();
+    }
+
+    /**
+     * Determine whether the user can delete the model.
+     */
+    public function delete(User $user, Borrowing $borrowing): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can restore the model.
+     */
+    public function restore(User $user, Borrowing $borrowing): bool
+    {
+        return false;
+    }
+
+    /**
+     * Determine whether the user can permanently delete the model.
+     */
+    public function forceDelete(User $user, Borrowing $borrowing): bool
+    {
+        return false;
+    }
+}
